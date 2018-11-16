69ee6e71097d51ce24729b90e70b0ef8e8131593
Vasiliy <vasiliy.mv.obzor@gmail.com>
2018-11-16 13:00:31 +0400

    Add new UI for main page & add FontAwesome 5

---
+++ frontend/src/components/content/HeroArticle.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="jumbotron jumbotron-fluid">
+    <div class="container">
+      <h1 class="display-4">Fluid jumbotron</h1>
+      <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+    </div>
+  </div>
+
+</template>
+
+<script>
+  export default {
+    name: "CarouselArticle"
+  }
+</script>
+
+<style scoped>
+
+</style>
